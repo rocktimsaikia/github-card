e58a7584d223f8cbd8cc5585d676d605a19ed466
Naveen <catchspider2002@gmail.com>
2020-04-19 03:21:29 +0530

    Added dark mode version and updated github logo with svg

--- src/widget.component.mjs
+++ src/widget.component.mjs
@@ -1,48 +1,51 @@
 import { widget_style } from "./widget.style.mjs";
 
 class myCard extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this._shadowRoot = this.attachShadow({ mode: 'open' });
-        this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+  }
 
-    };
+  connectedCallback() {
+    const font_uri = "https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400;600&family=Roboto&display=swap";
+    this.font_Loader(font_uri);
 
-    connectedCallback() {
-        const font_uri = 'https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400;600&family=Roboto&display=swap'
-        this.font_Loader(font_uri);
+    this.fetch_data(this.getAttribute("data-user")).then((res) => {
+      const card_template = this.create_card(res);
+      this._shadowRoot.querySelector(".card").innerHTML = card_template;
+      this._shadowRoot.querySelector(".card").setAttribute("data-theme", this.getAttribute("data-theme"));
+    });
+  }
 
-        this.fetch_data(this.getAttribute('data-user')).then(res => {
-            const card_template = this.create_card(res);
-            this._shadowRoot.querySelector('.card').innerHTML = card_template;
-        });
-    };
+  /** fetch the data */
+  async fetch_data(res) {
+    const data = await fetch(`https://api.github.com/users/${res}`, {
+      method: "GET",
+    });
+    const result = await data.json();
+    return result;
+  }
 
+  /** loads the fonts */
+  font_Loader(url) {
+    const font_link = document.createElement("link");
+    font_link.setAttribute("type", "text/css");
+    font_link.setAttribute("rel", "stylesheet");
+    font_link.setAttribute("href", url);
+    document.head.appendChild(font_link);
+  }
 
-    /** fetch the data */
-    async fetch_data(res) {
-        const data = await fetch(`https://api.github.com/users/${res}`, {
-            method: "GET"
-        });
-        const result = await data.json();
-        return result;
-    };
-
-    /** loads the fonts */
-    font_Loader(url) {
-        const font_link = document.createElement('link');
-        font_link.setAttribute('type', 'text/css');
-        font_link.setAttribute('rel', 'stylesheet');
-        font_link.setAttribute('href', url)
-        document.head.appendChild(font_link);
-    };
-
-    create_card(user) {
-        return `
+//   <a href="${user.html_url}" target="_blank" rel="noopener"><img id="github-logo" src="https://i.ibb.co/frv5pB3/github-logo.png" alt="github-logo" border="0"></a>
+  create_card(user) {
+    return `
+        <div class="card-outer">
         <div class="cover"></div>
         <div class="card-wrapper">
-        <a href="${user.html_url}" target="_blank" rel="noopener"><img id="github-logo" src="https://i.ibb.co/frv5pB3/github-logo.png" alt="github-logo" border="0"></a>
+        <a href="${user.html_url}" target="_blank" rel="noopener">
+            <svg id="github-logo" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>github-logo</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+        </a>
         <div class="card-header">
         <div class="card-img-wrapper"><img src="https://avatars.githubusercontent.com/${user.login}"/></div>
         <h1 class="card-title">${user.name}</h1>
@@ -64,11 +67,12 @@ class myCard extends HTMLElement {
             </div>
         </div>
         </div>
+        </div>
         `;
-    }
-};
+  }
+}
 
-const template = document.createElement('template');
+const template = document.createElement("template");
 template.innerHTML = `
     <style>
         ${widget_style}
@@ -76,6 +80,6 @@ template.innerHTML = `
     <div class="card">
         <img src="https://i.ibb.co/KDkTD3n/Ellipsis-1s-200px.gif" alt="Ellipsis-1s-200px" border="0">
     </div>
-`
+`;
 
-customElements.define('github-card', myCard);
+customElements.define("github-card", myCard);
--- src/widget.style.mjs
+++ src/widget.style.mjs
@@ -1,4 +1,20 @@
 export const widget_style = `
+:host {
+    --bg-color: #ffffff;
+    --title-color: #434343;
+    --resname-color: #646464;
+    --desc-color: rgba(0, 0, 0, 0.54);
+    --footer-color: rgba(0, 0, 0, 0.52);
+    --box-color: #EFEFEF;
+}
+[data-theme="dark"] {
+    --bg-color: #23262c;
+    --title-color: #e2e2e2;
+    --resname-color: #9b9b9b;
+    --desc-color: rgba(255, 255, 255, 0.54);
+    --footer-color: rgba(255, 255, 255, 0.52);
+    --box-color: #303237;
+}
 h1, div, p {
     margin: 0px;
     padding: 0px;
@@ -9,6 +25,7 @@ h1, div, p {
     position: absolute;
     top: 10px;
     right: 10px;
+    fill: var(--title-color);
 }
 .cover{
     height: 120px;
@@ -20,42 +37,36 @@ h1, div, p {
     border-top-left-radius: 5px;
     border-top-right-radius: 5px;
 }
-
 .card {
     position: relative;
     display: inline-block;
-    background: #ffffff;
-    border-radius: 5px;
     box-shadow:  0 12px 13px rgba(0,0,0,0.16), 0 12px 13px rgba(0,0,0,0.16);
     text-align: center;
-    padding: 20px 30px;
-    padding-top: 5px;
     transition: all 0.5s;
 }
-.card .fa-github {
-    position: absolute;
-    color: #646464;
-    font-size: 20px;
-    top: 10px;
-    right: 10px;
+.card .card-outer{
+    border-radius: 5px;
+    background: var(--bg-color);
+    padding: 20px 30px;
+    padding-top: 5px;
 }
 .card .card-title {
     font-family: 'Baloo Paaji 2', cursive;
-    color: #434343;
+    color: var(--title-color);
     margin-bottom: -8px;
     font-size: 30px;
     font-weight: 600;
 }
 .card .card-resname {
     margin-bottom: 20px;
-    color: #646464;
+    color: var(--resname-color);
 }
 .card .card-desc {
     width: 250px;
     margin: auto;
     display: block;
     font-weight: 600;
-    color: #0000008a;
+    color: var(--desc-color);
 }
 .card .card-img-wrapper {
     position: relative;
@@ -74,7 +85,7 @@ h1, div, p {
 }
 .card .card-footer .footer-box {
     position: relative;
-    background: #efefef;
+    background: var(--box-color);
     border-top: 2px solid #ff9b9b;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.26), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
     border-radius: 5px;
@@ -89,14 +100,14 @@ h1, div, p {
 }
 .card .card-footer .footer-box .box-wrapper .count {
     font-family: 'Baloo Paaji 2', cursive;
-    color: #434343;
+    color: var(--title-color);
     font-size: 25px;
     font-weight: 600;
 }
 .card .card-footer .footer-box .box-wrapper .box-text {
     font-size: 12px;
     font-weight: 600;
-    color: #00000085;
+    color: var(--footer-color);
     letter-spacing: 0.5px;
 }
 
@@ -105,4 +116,4 @@ h1, div, p {
         width: 100%;
     }
 }
-`
+`;
