c0c1671b77fa0fc67dbe3ea142be78d91aea883b
Rocktim Saikia <srocktim61@gmail.com>
2021-04-22 07:41:43 -1100

    refactor: replace xo with eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
-'use strict';
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-	entry: './lib/widget.component.mjs',
-	output: {
-		filename: 'widget.min.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	mode: 'production',
-	optimization: {
-		minimize: true,
-		minimizer: [new TerserPlugin()]
-	}
+  entry: './lib/widget.component.mjs',
+  output: {
+    filename: 'widget.min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 };
