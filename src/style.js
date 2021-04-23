ea1308fc1c64ea024e813e49e957bdde8cce855e
Rocktim Saikia <srocktim61@gmail.com>
2021-04-22 23:53:20 -1100

    style: update the stats font size

--- src/style.js
+++ src/style.js
@@ -120,7 +120,7 @@ a{
 .card .card-footer .footer-box .box-wrapper .count {
     font-family: 'consolas'
     color: #434343;
-    font-size: 22px;
+    font-size: 20px;
     font-weight: 600;
 }
 .card .card-footer .footer-box .box-wrapper .box-text {
