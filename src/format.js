eace1d877b8e222e67f91ef02e50f062654db4e8
rocktimsaikia <rocktimthedev@gmail.com>
2026-05-27 17:03:32 +0530

    build: replace ESLint with Biome for linting and formatting

--- src/format.js
+++ src/format.js
@@ -1,18 +1,17 @@
-/* eslint-disable no-restricted-properties */
 function nFormatter(value) {
-  if (!(value >= 1000)) return value;
-  let newValue = value;
-  const suffixes = ['', 'K', 'M', 'B', 'T'];
-  let suffixNum = 0;
-  while (newValue >= 1000) {
-    newValue /= 1000;
-    suffixNum += 1;
-  }
+	if (!(value >= 1000)) return value;
+	let newValue = value;
+	const suffixes = ['', 'K', 'M', 'B', 'T'];
+	let suffixNum = 0;
+	while (newValue >= 1000) {
+		newValue /= 1000;
+		suffixNum += 1;
+	}
 
-  newValue = newValue.toPrecision(3);
+	newValue = newValue.toPrecision(3);
 
-  newValue += suffixes[suffixNum];
-  return newValue;
+	newValue += suffixes[suffixNum];
+	return newValue;
 }
 
 export default nFormatter;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-/* eslint no-undef: 0 */
-import widgetStyle from './style.js';
-import nFormatter from './format.js';
 import escapeHtml from './escape.js';
+import nFormatter from './format.js';
+import widgetStyle from './style.js';
 
 const template = document.createElement('template');
 
@@ -13,56 +12,56 @@ template.innerHTML = `
 `;
 
 class myCard extends HTMLElement {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this._shadowRoot = this.attachShadow({ mode: 'open' });
-    this._shadowRoot.append(template.content.cloneNode(true));
-  }
+		this._shadowRoot = this.attachShadow({ mode: 'open' });
+		this._shadowRoot.append(template.content.cloneNode(true));
+	}
 
-  static get observedAttributes() {
-    return ['data-theme'];
-  }
+	static get observedAttributes() {
+		return ['data-theme'];
+	}
 
-  attributeChangedCallback(attr, oldValue, newValue) {
-    if (attr === 'data-theme' && oldValue !== newValue) {
-      this.setTheme(newValue);
-    }
-  }
+	attributeChangedCallback(attr, oldValue, newValue) {
+		if (attr === 'data-theme' && oldValue !== newValue) {
+			this.setTheme(newValue);
+		}
+	}
 
-  connectedCallback() {
-    this.fetchData(this.getAttribute('data-user')).then((response) => {
-      const cardTemplate = this.createCard(response);
-      this._shadowRoot.querySelector('.card').innerHTML = cardTemplate;
-    });
-  }
+	connectedCallback() {
+		this.fetchData(this.getAttribute('data-user')).then((response) => {
+			const cardTemplate = this.createCard(response);
+			this._shadowRoot.querySelector('.card').innerHTML = cardTemplate;
+		});
+	}
 
-  setTheme(theme) {
-    switch (theme) {
-      case 'dark':
-        this._shadowRoot.querySelector('.card').classList.add('dark');
-        break;
-      default:
-        this._shadowRoot.querySelector('.card').classList.remove('dark');
-        break;
-    }
-  }
+	setTheme(theme) {
+		switch (theme) {
+			case 'dark':
+				this._shadowRoot.querySelector('.card').classList.add('dark');
+				break;
+			default:
+				this._shadowRoot.querySelector('.card').classList.remove('dark');
+				break;
+		}
+	}
 
-  /** Fetch the data */
-  async fetchData(response) {
-    const data = await fetch(`https://api.github.com/users/${response}`, {
-      method: 'GET',
-    });
-    const responseult = await data.json();
-    return responseult;
-  }
+	/** Fetch the data */
+	async fetchData(response) {
+		const data = await fetch(`https://api.github.com/users/${response}`, {
+			method: 'GET',
+		});
+		const responseult = await data.json();
+		return responseult;
+	}
 
-  createCard(user) {
-    const login = escapeHtml(user.login);
-    const htmlUrl = escapeHtml(user.html_url);
-    const name = escapeHtml(user.name);
-    const bio = escapeHtml(user.bio ?? '');
-    return `
+	createCard(user) {
+		const login = escapeHtml(user.login);
+		const htmlUrl = escapeHtml(user.html_url);
+		const name = escapeHtml(user.name);
+		const bio = escapeHtml(user.bio ?? '');
+		return `
         <div class="cover"></div>
         <div class="card-wrapper">
         <a href="https://github.com/${login}" target="_blank" rel="noopener"><img id="github-logo" src="https://i.ibb.co/frv5pB3/github-logo.png" alt="github-logo" border="0"></a>
@@ -90,7 +89,7 @@ class myCard extends HTMLElement {
         </div>
         </div>
         `;
-  }
+	}
 }
 
 customElements.define('github-card', myCard);
