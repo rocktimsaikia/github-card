d01a70756f190f7aae9bba5f0aad6b46a2ad02a2
Rocktim <rocktimthedev@gmail.com>
2026-05-27 17:06:57 +0530

    build: replace ESLint with Biome for linting and formatting (#12)

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@ import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 export default [
-  {
-    input: 'src/index.js',
-    output: {
-      file: pkg.module,
-      format: 'es',
-    },
-    plugins: [uglify()],
-  },
+	{
+		input: 'src/index.js',
+		output: {
+			file: pkg.module,
+			format: 'es',
+		},
+		plugins: [uglify()],
+	},
 ];
--- src/escape.js
+++ src/escape.js
@@ -1,12 +1,12 @@
 // Escape user-controlled values (e.g. GitHub name/bio) before injecting them
 // into the card's innerHTML, to prevent HTML/attribute injection.
 function escapeHtml(value) {
-  return String(value)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
+	return String(value)
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&#39;');
 }
 
 export default escapeHtml;
